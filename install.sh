acb3e0022add2f7a8f1185e4d0d0d39a0320fe11
Mustard4868 <nynkelindeman@gmail.com>
2025-02-02 19:38:43 +0100

    add old file deletion

--- install.sh
+++ install.sh
@@ -18,6 +18,15 @@ curl -L -o "$RELEASE_TAG.tar.gz" "$DOWNLOAD_URL"
 # Install to Steam's compatibilitytools.d directory
 tar -xvf "$RELEASE_TAG.tar.gz" -C "$INSTALL_DIRECTORY"
 
+# Delete old versions
+read -p "Do you want to delete old versions? (y/N): " response
+if [[ "$response" == "y" ]]; then
+    find "$INSTALL_DIRECTORY" -maxdepth 1 -type d -name "GE-Proton*" ! -name "$RELEASE_TAG" -exec rm -rf {} +
+    echo "Old versions deleted"
+else
+    echo "Old versions not deleted"
+fi
+
 # Clean up
 rm -rf ./"$RELEASE_TAG.tar.gz"
 echo "Downloaded and installed $RELEASE_TAG"
